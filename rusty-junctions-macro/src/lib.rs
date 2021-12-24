8ecda4909a09719d9fe7342d806dfb0286f7af0e
Kyle Cotton <kylecottonkc@gmail.com>
2021-12-24 14:48:32 +0000

    Added `rusty-junctions-macro` as dependency
    Added the `rusty-junctions-macro` crate as a dependency.
    Currently there is a `macro_rules!` macros for defining the *when*
    part of a Join Pattern.

---
+++ rusty-junctions-macro/src/lib.rs
@@ -0,0 +1,7 @@
+/// Macro for defining the channels that are part of the Join Pattern
+#[macro_export]
+macro_rules! when {
+    ( $junction:ident; $initial_channel:ident, $( $other_channels:ident ),* ) => {
+        $junction.when(&$initial_channel)$( .and(&$other_channels) )*
+    }
+}
