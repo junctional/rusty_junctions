b7fab364b39f9d57d665e33fe05d4813ab3a73f0
Kyle Cotton <kylecottonkc@gmail.com>
2022-01-10 23:36:43 +0000

    Created new macro and utils crate.
    Refactored the `when!` macro to a separate crate. This will allow for
    the later more complex and project structure restricting procedural
    macros, to also be defined in this crate.
    
    All of the macros will then be re-exported to give a single dependency
    to `rusty-junctions`.

--- rusty-junctions-macro/src/lib.rs
+++ rusty-junctions-macro/src/lib.rs
@@ -1,7 +1,1 @@
-/// Macro for defining the channels that are part of the Join Pattern
-#[macro_export]
-macro_rules! when {
-    ( $junction:ident; $initial_channel:ident, $( $other_channels:ident ),* ) => {
-        $junction.when(&$initial_channel)$( .and(&$other_channels) )*
-    }
-}
+pub use rusty_junctions_utils::when;
