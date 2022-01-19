f2188fe664bd60a1034dee2de2987f285a590629
Kyle Cotton <kylecottonkc@gmail.com>
2022-01-19 10:51:57 +0000

    Updated the `rusty-junctions-macro` crate
    Added the `rusty-junctions-partial-pattern-derive` crate as a
    dependency, and re-exported its proc-macro to the crate root.

--- rusty-junctions-macro/src/lib.rs
+++ rusty-junctions-macro/src/lib.rs
@@ -2,3 +2,4 @@ pub use rusty_junctions_utils::when;
 pub use rusty_junctions_function_transformations::function_transform;
 pub use rusty_junctions_function_types::function_types;
 pub use rusty_junctions_join_pattern_derive::JoinPattern;
+pub use rusty_junctions_partial_pattern_derive::TerminalPartialPattern;
