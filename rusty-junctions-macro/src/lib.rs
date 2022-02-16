ca1f961620bb5fa842d2fae9df3514737707ebbc
Kyle Cotton <kylecottonkc@gmail.com>
2022-02-16 12:38:08 +0000

    Refactored Procedural Macro Library Generation
    Refactored the `rusty-junctions-library-macro` to now be under the
    `rusty-junctions-macro` crate.
    Allows for a single dependency to be added to the `rusty-junctions`
    project.

--- rusty-junctions-macro/src/lib.rs
+++ rusty-junctions-macro/src/lib.rs
@@ -1,4 +1,10 @@
 pub mod client {
+    pub use client_api_macro::{channel_def, junction_dec, when};
     pub use client_api_proc_macro::junction;
-    pub use client_api_macro::{when, channel_def, junction_dec};
+}
+
+pub mod library {
+    pub use library_generation_proc_macro::{
+        library_generate as generate, JoinPattern, PartialPattern, TerminalPartialPattern,
+    };
 }
